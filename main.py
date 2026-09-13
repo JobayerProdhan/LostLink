42cef2bee873f0179b02bcd880e17994bd605a1d
Jobayer Prodhan <jobayerprodhan08@gmail.com>
2026-09-13 16:29:25 +0600

    all done

--- main.py
+++ main.py
@@ -1,74 +1,164 @@
 from enum import Enum 
-from fastapi import FastAPI ,HTTPException  
+from fastapi import FastAPI ,HTTPException ,Request ,Form
 from pydantic import BaseModel 
+from fastapi.templating import Jinja2Templates 
+from fastapi.staticfiles import StaticFiles 
+from starlette.middleware.sessions import SessionMiddleware 
+from pwdlib  import PasswordHash 
+from fastapi.responses import RedirectResponse 
 
 app = FastAPI() 
- 
- 
-class ItemStatus(str,Enum):
-    lost = "lost" 
-    found = "found" 
 
-class Item(BaseModel):
-    name:str 
-    category:str
-    location:str
-    date:str 
-    description:str 
-    status : ItemStatus 
+templates = Jinja2Templates(directory='templates')
+app.mount("/static",StaticFiles(directory="static"),name="static")
+
+app.add_middleware(
+    SessionMiddleware,
+    secret_key='change-this-secret-key'
+)
+
+password_hash = PasswordHash.recommended() 
+
+users = {}
 
 items_db = [] 
 
-# Request body --> create an item 
-@app.post("/items/")
-async def create_item(item:Item):
-    items_db.append(item) 
-    return item 
+@app.get("/")
+async def home(request: Request):
+    username = request.session.get('username') 
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={"request": request,
+                 "username":username}
+    )
 
+@app.get("/register")
+async def register_page(request:Request):
+    return templates.TemplateResponse(
+        request= request,
+        name="register.html"
+    )
 
-# Qurey Parameter --> Search/filter items 
-@app.get("/items/") 
-async def get_items(
-    category:str | None =  None ,
-    location:str | None = None,
-    status:ItemStatus | None = None
+@app.post("/register")
+async def register(
+    request:Request,
+    username:str = Form(...),
+    password:str = Form(...)
 ):
-    results = items_db 
+    if username  in users:
+        return templates.TemplateResponse(
+            request=request,
+            name='register.html',
+            context={
+                "error":"Username  already exists"
+            }
+        )
     
-    if  category:
-        results = [item for item in results if item.category == category]
+    hashed_password = password_hash.hash(password) 
+    users[username] =  hashed_password 
     
+    return RedirectResponse(
+        url="/login",
+        status_code=303
+    )
+
+@app.get("/login")
+async  def  login_page(request:Request):
+    return templates.TemplateResponse (
+        request=request,
+        name="login.html"
+    )
+        
+@app.post("/login")
+async def login(
+    request:Request,
+    username:str = Form(...),
+    password:str= Form(...)
+):
+    if username not in users:
+        return templates.TemplateResponse(
+            request=request,
+            name="login.html",
+            context={
+                "error": "Invalid username or password"
+            }
+        ) 
+    if not password_hash.verify(
+        password,
+        users[username]
+    ):
+
+        return templates.TemplateResponse(
+            request=request,
+            name="login.html",
+            context={
+                "error": "Invalid username or password"
+            }
+        )
+
+    request.session["username"] = username
+
+    return RedirectResponse(
+        url="/",
+        status_code=303
+    )
+
+@app.get("/create") 
+async def create_page(request:Request):
+    return templates.TemplateResponse(
+        request= request,
+        name='create.html',
+        context = {'request': request}
+    )
+
+
+@app.post("/items/") 
+async def create_item(
+    name:str = Form(...),
+    category: str = Form(...),
+    location: str = Form(...),
+    date: str = Form(...),
+    description: str = Form(...),
+    status: str = Form(...)
+):
+    item = {
+        "name": name,
+        "category": category,
+        "location": location,
+        "date": date,
+        "description": description,
+        "status": status
+    }
+
+    items_db.append(item)
+
+    return item
+
+
+@app.get("/search")
+async def search_page(
+    request: Request,
+    category: str | None = None,
+    location: str | None = None,
+    status: str | None = None
+):
+    results = items_db
+
+    if category:
+        results = [item for item in results
+                   if item["category"] == category]
+
     if location:
-        results = [item for item in results if item.location == location] 
-    
+        results = [item for item in results
+                   if item["location"] == location]
+
     if status:
-        results = [item for item in results if item.status == status] 
+        results = [item for item in results
+                   if item["status"] == status]
     
-    return results 
-
-# path parameter --> get specific  item 
-@app.get("/itms/{items_id}")
-async def get_items(items_id:int):
-    if items_id >= len(items_db):
-        raise HTTPException (status_code=404,detail="Items not found") 
-
-
-
-# put --> Replace the entire item  
-@app.put("/items/{items_id}") 
-async def update(item_id:int , item:Item):
-    if item_id>=len(items_db):
-        raise HTTPException(status_code=404,detail='Items not  found') 
-    
-    items_db[item_id] = item
-    return items_db[item_id] 
-
-
-# DELETE --> Delete an item 
-@app.delete("/items/{items_id}")
-async def delete_item(item_id:int):
-    if item_id >= len(items_db):
-        raise HTTPException(status_code=404, detail="Item not found")
-    delete_item = items_db.pop(item_id) 
-    return {"message":"item deleted",
-            "item": delete_item}
+    return templates.TemplateResponse(
+        request= request,
+        name = "search.html",
+        context={"request":request,"result": results}
+    )
